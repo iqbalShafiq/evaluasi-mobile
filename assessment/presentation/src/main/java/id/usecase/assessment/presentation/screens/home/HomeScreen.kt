3a2ea7040808b4783eefd0d127a0a9b6b2974514
Iqbal Shafiq <iqbal.syafiq.rozaan@gmail.com>
2025-01-11 13:35:54 +0700

    fix: add close button on search result

--- assessment/presentation/src/main/java/id/usecase/assessment/presentation/screens/home/HomeScreen.kt
+++ assessment/presentation/src/main/java/id/usecase/assessment/presentation/screens/home/HomeScreen.kt
@@ -19,11 +19,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -141,10 +142,14 @@ fun HomeScreen(
                             placeholder = { Text("Hinted search text") },
                             leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
                             trailingIcon = {
-                                Icon(
-                                    Icons.Default.MoreVert,
-                                    contentDescription = null
-                                )
+                                IconButton(
+                                    onClick = { expanded = false }
+                                ) {
+                                    Icon(
+                                        Icons.Rounded.Close,
+                                        contentDescription = "Cross",
+                                    )
+                                }
                             },
                         )
                     },
