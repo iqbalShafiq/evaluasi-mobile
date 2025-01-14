108b1a132f3d2806dcd71dbe0adda3f86198f736
syafiq <iqbal.syafiq.rozaan@gmail.com>
2025-01-14 22:48:55 +0700

    fix: analytics di

--- assessment/data/src/main/java/id/usecase/assessment/data/di/assessmentRepositoryModule.kt
+++ assessment/data/src/main/java/id/usecase/assessment/data/di/assessmentRepositoryModule.kt
@@ -1,10 +1,12 @@
 package id.usecase.assessment.data.di
 
+import id.usecase.assessment.data.AnalyticsRepositoryImpl
 import id.usecase.assessment.data.AssessmentRepositoryImpl
 import id.usecase.assessment.data.CategoryRepositoryImpl
 import id.usecase.assessment.data.ClassRoomRepositoryImpl
 import id.usecase.assessment.data.EventRepositoryImpl
 import id.usecase.assessment.data.StudentRepositoryImpl
+import id.usecase.assessment.domain.AnalyticsRepository
 import id.usecase.assessment.domain.AssessmentRepository
 import id.usecase.assessment.domain.CategoryRepository
 import id.usecase.assessment.domain.ClassRoomRepository
@@ -43,6 +45,13 @@ val assessmentRepositoryModule = module {
 
     single<StudentRepository> {
         StudentRepositoryImpl(
+            dataSource = get(),
+            dispatcher = get(),
+        )
+    }
+
+    single<AnalyticsRepository> {
+        AnalyticsRepositoryImpl(
             dataSource = get(),
             dispatcher = get(),
         )
